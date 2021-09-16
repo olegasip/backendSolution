fa2fd899b87c5acee89a0037d38a41c62bf9aa05
olegasip <olegasip.ua@gmail.com>
2021-09-16 18:09:52 +0300

    + four task (r)

--- src/Package/PressureFour.java
+++ src/Package/PressureFour.java
@@ -2,16 +2,11 @@ package Package;
 
 public class PressureFour {
     public static boolean isDivisible(int wallLength, int pixelSize) {
-        if(wallLength % pixelSize == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return wallLength % pixelSize == 0;
     }
 
     public static void main(String[] args) {
-        System.out.println("Result: " + isDivisible(4050,27));
+        System.out.println("Result: " + isDivisible(4066,27));
     }
 
 }
